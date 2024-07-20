239cb8ae1cd9cf25e696fabd1f6f3465dfafc61c
Ali Haghshenas <alihaghshenas821@gmail.com>
2024-07-20 15:21:50 +0330

    Add project files.

---
+++ DataStructure/Classes/Search.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Classes
+{
+    public class Search
+    {
+        public int? Linear(int[] array, int key) 
+        {
+            for (int i = 0; i != array.Length -1; i++)
+            {
+                if (array[i] == key)
+                    return i;
+            }
+
+            return null;
+        }
+
+        public int? Binary(int[] array, int key) {
+            int min = 0;
+            int max = array.Length-1;
+
+            while (min <= max) 
+            { 
+                int mid = (min+max)/2;
+
+                if (array[mid] == key)
+                {
+                    return mid;
+                }
+                else if (array[mid] < key)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using DataStructure.Classes;
+
+int[] numbers = new int[] { 8, 10, 15, 25, 89, 90, 92, 93, 99, 100};
+
+Console.WriteLine(new Search().Binary(numbers,12));
